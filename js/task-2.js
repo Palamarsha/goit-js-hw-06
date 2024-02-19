4f5a52c7424a80fcecc3d6091ba36774683e0eca
Palamarsha <149620290+Palamarsha@users.noreply.github.com>
2024-02-19 17:35:58 +0200

    updste task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,17 +1,17 @@
 class Storage {
-    #items = [];
+    #items;
     constructor(items) {
-        this.#items = [...items];
+        this.#items = items;
     }
 
     getItems() {
         return this.#items;
     }
     addItem(newItem) {
-      return  this.#items.push(newItem);
+     this.#items.push(newItem);
     }
     removeItem(itemToRemove) {
-        this.#items = this.#items.splice(item => item === itemToRemove);
+        this.#items = this.#items.filter(item => item !== itemToRemove);
     }
 }
 
